7d0bf7933a3b789ea4b84d2893bfe75f71330c07
Tony Naughton <tonynaughton1@gmail.com>
2021-03-01 15:50:38 +0000

    Basic implementation of settings and dotenv

--- routes.js
+++ routes.js
@@ -1,14 +1,16 @@
-const accounts = require("./app/controllers/accounts");
+const Accounts = require("./app/controllers/accounts");
 const stadiums = require('./app/controllers/stadiums');
 
 module.exports = [
-    { method: "GET", path: "/", config: accounts.index },
-    { method: "GET", path: "/signup", config: accounts.showSignup },
-    { method: "GET", path: "/login", config: accounts.showLogin },
-    { method: "GET", path: "/logout", config: accounts.logout },
+    { method: "GET", path: "/", config: Accounts.index },
+    { method: "GET", path: "/signup", config: Accounts.showSignup },
+    { method: "GET", path: "/login", config: Accounts.showLogin },
+    { method: "GET", path: "/logout", config: Accounts.logout },
+    { method: 'GET', path: '/settings', config: Accounts.showSettings },
+    { method: 'POST', path: '/settings', config: Accounts.updateSettings },
 
-    { method: "POST", path: "/signup", config: accounts.signup },
-    { method: "POST", path: "/login", config: accounts.login },
+    { method: "POST", path: "/signup", config: Accounts.signup },
+    { method: "POST", path: "/login", config: Accounts.login },
 
     { method: 'GET', path: '/home', config: stadiums.index },
     { method: 'GET', path: '/add-stadium', config: stadiums.addStadiumView },
