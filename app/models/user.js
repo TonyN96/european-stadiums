e2edeb6332e16482b42d1d2584217541ff63f872
Tony Naughton <tonynaughton1@gmail.com>
2021-05-13 18:03:40 +0100

    Password hashing using bcrypt

--- app/models/user.js
+++ app/models/user.js
@@ -3,25 +3,26 @@
 const Mongoose = require("mongoose");
 const Schema = Mongoose.Schema;
 const Boom = require("@hapi/boom");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    admin: Boolean
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  admin: Boolean,
 });
 
 userSchema.statics.findByEmail = function (email) {
-    return this.findOne({ email: email });
+  return this.findOne({ email: email });
 };
 
-userSchema.methods.comparePassword = function (candidatePassword) {
-    const isMatch = this.password === candidatePassword;
-    if (!isMatch) {
-        throw Boom.unauthorized('Password mismatch');
-    }
-    return this;
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  if (!isMatch) {
+    throw Boom.unauthorized("Password mismatch");
+  }
+  return this;
 };
 
 module.exports = Mongoose.model("User", userSchema);
